76689777432ffe3ed655e6f63b600f41dbf5d1ab
sujitht97 <48566466+sujitht97@users.noreply.github.com>
2020-04-04 21:31:32 +0530

    Update WebSecurityConfig.java

--- src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -16,6 +16,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.authorizeRequests()
+				.antMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+				.and()
 			.formLogin()
 				.loginPage("/login")
 				.permitAll()
